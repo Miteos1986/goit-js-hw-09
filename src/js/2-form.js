a65eaacb0ba9b8958c90af24bf32d02cfd55481c
Miteos1986 <mdmitriy1986@gmail.com>
2024-03-25 17:46:20 +0100

    2 task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
+const STORAGE_KEY = "feedback-form-state";
+
 const form = document.querySelector(".feedback-form");
+const textarea = form.querySelector("textarea");
+const input = form.querySelector("input");
+
+form.addEventListener("submit", handleSubmit);
+textarea.addEventListener("input", onTextareaInput);
+
+input.addEventListener("input", onTextareaInput);
+
+enteredTextarea ();
+enteredEmail ();
+
+
+
+
+function onTextareaInput(event) {
+    const message = event.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+}
+function onInputInput(event){
+    const email = event.target.value;
+    localStorage.setItem(STORAGE_KEY, email);
+}
+
+function enteredTextarea (){
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+        if (savedMessage) textarea.value = savedMessage;
+    
+}
+function enteredEmail (){
+    const savedEmail = localStorage.getItem(STORAGE_KEY);
+        if (savedEmail) input.value = savedEmail;
+    
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    console.log("send");
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+}
